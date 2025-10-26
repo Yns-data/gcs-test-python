e9ed606c537390bdcf99e440ea98070ba6da8c7a
yns-data <younesessoualhi@gmail.com>
2025-10-26 14:03:07 +0000

    updating extraction file

--- test-gcs-storage.py
+++ test-gcs-storage.py
@@ -3,9 +3,9 @@ import pandas as pd
 from io import BytesIO
 
 # ⚙️ Configuration
-BUCKET_NAME = "mon-bucket"
-SOURCE_BLOB_NAME = "data/input.csv"     # chemin du CSV source dans le bucket
-DESTINATION_BLOB_NAME = "data/output.csv"  # chemin du CSV modifié
+BUCKET_NAME = "airfrance-bucket"
+SOURCE_BLOB_NAME = "df_call_parameters.csv"     # chemin du CSV source dans le bucket
+DESTINATION_BLOB_NAME = "df_call_parameters.csv"  # chemin du CSV modifié
 
 def read_csv_from_gcs(bucket_name: str, blob_name: str) -> pd.DataFrame:
     """Télécharge un CSV depuis un bucket GCS et le charge dans un DataFrame pandas."""
@@ -19,17 +19,14 @@ def read_csv_from_gcs(bucket_name: str, blob_name: str) -> pd.DataFrame:
     return df
 
 def write_csv_to_gcs(df: pd.DataFrame, bucket_name: str, blob_name: str):
-    """Écrit un DataFrame pandas vers un fichier CSV dans GCS."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
-    # Conversion du DataFrame en bytes
     csv_buffer = BytesIO()
     df.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)
 
-    # Upload vers GCS
     blob.upload_from_file(csv_buffer, content_type="text/csv")
     print(f"✅ CSV modifié uploadé vers '{blob_name}'.")
 
@@ -40,8 +37,12 @@ def modify_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 def main():
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    
+
     df = read_csv_from_gcs(BUCKET_NAME, SOURCE_BLOB_NAME)
-    df_modified = modify_dataframe(df)
+    df_modified = df[["destination"]]
     write_csv_to_gcs(df_modified, BUCKET_NAME, DESTINATION_BLOB_NAME)
 
 if __name__ == "__main__":
